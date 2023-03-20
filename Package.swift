15a1ae41e6375f172d39e7eb1062579920fc074d
wdd <parker>
2023-03-20 12:10:38 +0800

    0.1.42

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+  name: "XrayKit",
+  platforms: [.iOS(.v16)],
+  products: [
+    .library(name: "XrayKit", targets: ["XrayKit"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "XrayKit",
+      url: "https://github.com/Cross-Kit/XrayKit/releases/download/0.1.42/XrayKit.xcframework.zip",
+      checksum: "a54cec295418d529105212d9194d0b1a1f2907ad9e737f28627294f0638b3430"
+    )
+  ]
+)
